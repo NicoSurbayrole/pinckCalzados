faf8d7af742da87f0561ab18eda7145b092610ac
nicolas <nico.surba@hotmail.com>
2025-10-06 13:30:21 -0300

    estructura base del proyecto

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import NavBar from './components/NavBar.jsx';
+import ItemListContainer from './components/ItemListContainer.jsx';
+
+function App() {
+
+  return (
+    <>
+      <header>
+        <NavBar/>
+      </header>
+      <main>
+        <ItemListContainer/>
+      </main>
+    </>
+  )
+}
+
+export default App
